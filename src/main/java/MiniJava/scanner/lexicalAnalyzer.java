c71a0117b135fea7101db37f17b68c63aab9f3dd
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2024-08-19 01:24:04 +0330

    implemented self capsulated field in scanner

--- src/main/java/MiniJava/scanner/lexicalAnalyzer.java
+++ src/main/java/MiniJava/scanner/lexicalAnalyzer.java
@@ -3,10 +3,14 @@ package MiniJava.scanner;
 import MiniJava.errorHandler.ErrorHandler;
 import MiniJava.scanner.token.Token;
 import MiniJava.scanner.type.Type;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Setter
+@Getter
 public class lexicalAnalyzer {
     private Matcher matcher;
 
@@ -19,25 +23,25 @@ public class lexicalAnalyzer {
         for (Type tokenType : Type.values())
             tokenPattern.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
         Pattern expression = Pattern.compile(tokenPattern.substring(1));
-        matcher = expression.matcher(input.toString());
+        setMatcher(expression.matcher(input.toString()));
     }
 
     public Token getNextToken() {
 
-        while (matcher.find()) {
+        while (getMatcher().find()) {
             for (Type t : Type.values()) {
 
-                if (matcher.group(t.name()) != null) {
-                    if (matcher.group(Type.COMMENT.name()) != null) {
+                if (getMatcher().group(t.name()) != null) {
+                    if (getMatcher().group(Type.COMMENT.name()) != null) {
                         break;
 
                     }
-                    if (matcher.group(Type.ErrorID.name()) != null) {
+                    if (getMatcher().group(Type.ErrorID.name()) != null) {
                         ErrorHandler.printError("The id must start with character");
                         break;
                     }
 
-                    return new Token(t, matcher.group(t.name()));
+                    return new Token(t, getMatcher().group(t.name()));
                 }
             }
         }
